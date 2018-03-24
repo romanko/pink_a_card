b0aba29edc6661b7795af3344291e749fda14743
vadim <vadim.romanko@gmail.com>
2018-03-24 13:23:26 +0200

    Goods

---
+++ pick_a_card.rb
@@ -0,0 +1,9 @@
+values = ['2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', 'A']
+suits  = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+
+puts values[rand(9)]+" of "+suits[rand(4)]
+
+puts "#{values.sample} of #{suits.sample}"
+
+
+puts "Good."
